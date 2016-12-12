5804ba5af2ceba9a6cdb00f9bd0e8f10d15a3253
MuchContact <liuhengjiangis@foxmail.com>
2016-12-12 23:12:42 +0800

    echo; adb

---
+++ echo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Before starting subshell"
+(
+   count=1
+   while [ $count -le 10 ]
+   do
+       echo "$count"
+       sleep 1
+       (( count++ ))
+   done
+)
+echo "Finished"
